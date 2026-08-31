af73207df19219a17fab6662a3efe95e970dbd33
Sophia Arsie <sophia.arsie@escola.pr.gov.br>
2026-08-31 12:05:29 -0300

    Correct attack method and game loop in ataque.py
    Fix errors in attack method and game loop logic.

--- ataque.py
+++ ataque.py
@@ -5,4 +5,26 @@ class pokemon
   self.vida = vida
   self.ataque = ataque
   def atacar (self, inimigo):
-  dano: random.randing(self, ataque\\2, self.ataque
+  dano: random.randing(self.ataque//2, self.ataque
+    print(self.nome, "Atacou", inimigo.nome)
+    print("Dano", dano)
+    inimigo(vida = inimigo.nome)
+
+if inimigo.vida < 0
+    inimigo.vida = 0
+
+print("Vida de", inimigo.nome,":", inimigo.vida
+      print()
+pikachu = pokemon("Pikachu", 100, 20)
+Charmander = Pokemon("Charmander", 100, 25)
+while pikachu.vida > and charmander.vida > 0
+   pikachu.atacar(charmander)
+if charmander.vida < = 0:
+  prit("Charmander foi derrotado")
+  print("Pikachu venceu")
+  break
+  charmander.atacar(pikachu)
+  if pikachu < = 0:
+    print("Pikachu" foi derrotado!)
+    print("Charmander venceu!")
+break
