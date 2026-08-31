3b6c857dde104cecc68b1ec9c6615ac39d9c53d9
Sophia Arsie <sophia.arsie@escola.pr.gov.br>
2026-08-31 10:50:40 -0300

    Create initial Pokemon class with attack method

---
+++ ataque.py
@@ -0,0 +1,8 @@
+import random
+class pokemon
+  def_init_(self, nome, vida, ataque):
+  self.nome = nome
+  self.vida = vida
+  self.ataque = ataque
+  def atacar (self, inimigo):
+  dano: random.randing(self, ataque\\2, self.ataque
